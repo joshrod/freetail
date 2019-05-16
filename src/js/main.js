e0afa8f24f711aa6d8cbcfbac531f0520f682c02
Joshua Rodriguez <j_rodriguez@m110.local>
2019-05-16 15:46:45 -0500

    Core section now responsive for all devices (except landscape). Construction of seasonal beers has begun.

--- src/js/main.js
+++ src/js/main.js
@@ -21,9 +21,43 @@ window.onload = () => {
 		triggerHook: "onLeave",
 		duration: "100%"
 	})
-		.setTween(tween)
-		.addIndicators({ name: "tween" })
-		.addTo(controller);
+	.setTween(tween)
+	.addIndicators({ name: "tween" })
+	.addTo(controller);
+
+	// $('.beer-carousel').slick({
+	// 	dots: false,
+	// 	arrows: true,
+	// 	infinite: true,
+	// 	speed: 300,
+	// 	slidesToShow: 4,
+	// 	slidesToScroll: 4,
+	// 	responsive: [
+	// 		{
+	// 		breakpoint: 1024,
+	// 		settings: {
+	// 			slidesToShow: 4,
+	// 			slidesToScroll: 4,
+	// 			infinite: true,
+	// 			dots: false
+	// 		}
+	// 		},
+	// 		{
+	// 		breakpoint: 600,
+	// 		settings: {
+	// 			slidesToShow: 2,
+	// 			slidesToScroll: 2
+	// 		}
+	// 		},
+	// 		{
+	// 		breakpoint: 480,
+	// 		settings: {
+	// 			slidesToShow: 1,
+	// 			slidesToScroll: 1
+	// 		}
+	// 		}
+	// 	]
+	// });
 
 	window.onscroll = () => {
 		shrinkNav();
