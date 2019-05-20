dcf4d88738d7e56523b18023029e5a3770841264
Joshua Rodriguez <j_rodriguez@m110.local>
2019-05-20 17:48:37 -0500

    Breakpoints fixed for slick carousel

--- src/js/main.js
+++ src/js/main.js
@@ -26,7 +26,7 @@ window.onload = () => {
 		.addTo(controller);
 
 	$(".beer-carousel").slick({
-		arrows: true,
+		dots: false,
 		infinite: true,
 		speed: 300,
 		slidesToShow: 4,
@@ -35,21 +35,14 @@ window.onload = () => {
 			{
 				breakpoint: 1024,
 				settings: {
-					slidesToShow: 4,
-					slidesToScroll: 4,
+					slidesToShow: 2,
+					slidesToScroll: 2,
 					infinite: true,
 					dots: false
 				}
 			},
 			{
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 2
-				}
-			},
-			{
-				breakpoint: 480,
+				breakpoint: 680,
 				settings: {
 					slidesToShow: 1,
 					slidesToScroll: 1
